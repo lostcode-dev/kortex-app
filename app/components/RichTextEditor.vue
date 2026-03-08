41c5edc1718908841098fb51244a98816f0ed9e2
Daniel Soares <danielsoaresf@hotmail.com>
2026-03-08 09:43:15 -0300

    fix: update Quill editor stylesheet to use bubble theme

--- app/components/RichTextEditor.vue
+++ app/components/RichTextEditor.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import '@vueup/vue-quill/dist/vue-quill.bubble.css'
 
 const props = withDefaults(defineProps<{
   modelValue?: string
